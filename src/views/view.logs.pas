4d242b98fcf0b2a0e469f6acd08fec6b5d29ce0a
Eduardo Adão Locks <122515706+adao-locks@users.noreply.github.com>
2025-04-28 18:04:06 -0300

    Translations
    Logs form translated to portuguese

--- src/views/view.logs.pas
+++ src/views/view.logs.pas
@@ -8,7 +8,7 @@ uses
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, view.base.lists, Data.DB,
   System.ImageList, Vcl.ImgList, Vcl.Grids, Vcl.DBGrids, Vcl.WinXPanels,
   Vcl.Buttons, Vcl.StdCtrls, Vcl.ExtCtrls, Service.Register, Vcl.WinXCtrls,
-  Provider.Utils;
+  Provider.Utils, Vcl.ComCtrls;
 
 type
   TviewLogs = class(TviewBaseLists)
@@ -17,6 +17,10 @@ type
     edtNameAsk: TSearchBox;
     SearchBox1: TSearchBox;
     Label1: TLabel;
+    lblDateRegAsk: TLabel;
+    lblDateBirthAsk: TLabel;
+    edtDateReg: TDateTimePicker;
+    edtDateReg2: TDateTimePicker;
     procedure btnCloseWindowClick(Sender: TObject);
     procedure SpeedButton1Click(Sender: TObject);
     procedure btnConsultClick(Sender: TObject);
@@ -43,7 +47,75 @@ end;
 procedure TviewLogs.btnConsultClick(Sender: TObject);
 begin
   inherited;
-  Get_Logs;
+
+
+  ServiceRegister.QRYEntity.SQL.Text := 'SELECT * FROM PEOPLE WHERE 1=1';
+
+  DateRegistered1 := edtDateReg.Date;
+  DateRegistered2 := edtDateReg2.Date;
+
+  ServiceRegister.QRYEntity.SQL.Add
+    (' AND DATE_REGISTER BETWEEN :DATEI AND :DATEF');
+  ServiceRegister.QRYEntity.ParamByName('DATEI').AsDate := DateRegistered1;
+  ServiceRegister.QRYEntity.ParamByName('DATEF').AsDate := DateRegistered2;
+
+  if edtNameAsk.Text <> '' then
+  begin
+    ServiceRegister.QRYEntity.SQL.Add(' AND NAME LIKE :NAME');
+    ServiceRegister.QRYEntity.ParamByName('NAME').AsString :=
+      '%' + edtNameAsk.Text + '%';
+  end;
+
+  if edtFantasyAsk.Text <> '' then
+  begin
+    ServiceRegister.QRYEntity.SQL.Add(' AND FANTASY LIKE :FANTASY');
+    ServiceRegister.QRYEntity.ParamByName('FANTASY').AsString :=
+      '%' + edtFantasyAsk.Text + '%';
+  end;
+
+  if edtPhoneAsk.Text <> '' then
+  begin
+    ServiceRegister.QRYEntity.SQL.Add(' AND PHONE LIKE :PHONE');
+    ServiceRegister.QRYEntity.ParamByName('PHONE').AsString :=
+      '%' + edtPhoneAsk.Text + '%';
+  end;
+
+  if edtEmailAsk.Text <> '' then
+  begin
+    ServiceRegister.QRYEntity.SQL.Add(' AND EMAIL LIKE :EMAIL');
+    ServiceRegister.QRYEntity.ParamByName('EMAIL').AsString :=
+      '%' + edtEmailAsk.Text + '%';
+  end;
+
+  if edtZIPAsk.Text <> '' then
+  begin
+    ServiceRegister.QRYEntity.SQL.Add(' AND ZIPCODE LIKE :ZIPCODE');
+    ServiceRegister.QRYEntity.ParamByName('ZIPCODE').AsString :=
+      '%' + edtZIPAsk.Text + '%';
+  end;
+
+  if edtStateAsk.Text <> '' then
+  begin
+    ServiceRegister.QRYEntity.SQL.Add(' AND STATE LIKE :STATE');
+    ServiceRegister.QRYEntity.ParamByName('STATE').AsString :=
+      '%' + edtStateAsk.Text + '%';
+  end;
+
+  if edtCityAsk.Text <> '' then
+  begin
+    ServiceRegister.QRYEntity.SQL.Add(' AND CITY LIKE :CITY');
+    ServiceRegister.QRYEntity.ParamByName('-').AsString :=
+      '%' + edtCityAsk.Text + '%';
+  end;
+
+  if edtStreetAsk.Text <> '' then
+  begin
+    ServiceRegister.QRYEntity.SQL.Add(' AND STREET LIKE :STREET');
+    ServiceRegister.QRYEntity.ParamByName('STREET').AsString :=
+      '%' + edtStreetAsk.Text + '%';
+  end;
+
+  ServiceRegister.QRYEntity.Open;
   ResizeDBGridColumns(DBGData);
   AlighDBGridColumns(DBGData);
 end;
